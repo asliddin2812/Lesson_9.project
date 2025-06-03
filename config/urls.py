95133e2753fdb89dea232e10d7c7cd55bf4970b3
Asliddin Mamatov <169055620+asliddin2812@users.noreply.github.com>
2025-06-03 16:02:00 +0500

    L10

--- config/urls.py
+++ config/urls.py
@@ -21,11 +21,6 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-# Simple_jwt
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -45,8 +40,6 @@ urlpatterns = [
    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('',include('app_silovar.urls')),
 ]
